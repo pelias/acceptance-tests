5be693d6a89ce6312c03b575ca1c2546117b96a1
Severyn Kozak <severyn.kozak@gmail.com>
2015-03-09 11:03:18 -0400

    Define prettyPrintResult().
    test.js
    	-Define `prettyPrintResult()`, to prettily format and print
    	individual test results.

--- test.js
+++ test.js
@@ -1,13 +1,58 @@
 var locations = require( './test_cases/locations.json' );
 var querystring = require( 'querystring' );
 var supertest = require( 'supertest' );
+var colors = require( 'colors' );
+var util = require( 'util' );
 
 var testSuites = [ require( './test_cases/search.json' ) ];
 testSuites.forEach( function ( suite ){
-  execTestSuite( suite, function ( results ){
-    console.log( JSON.stringify( results, undefined, 4 ) );
+  var startTime = new Date().getTime();
+  execTestSuite( suite, function ( testResults ){
+    var timeTaken = new Date().getTime() - startTime;
+    testResults.results.sort( function ( a, b ){
+      return (a.testCase.id > b.testCase.id) ? 1 : -1;
+    });
+    testResults.results.forEach( function ( result ){
+      prettyPrintResult( result );
+    });
   });
 });
+
+function prettyPrintResult( result ){
+  switch( result.result ){
+    case 'pass':
+      var output = util.format(
+        '✔ [%s] "%s"', result.testCase.id, result.testCase.in.input
+      ).green;
+      console.log( output );
+      break;
+
+    case 'fail':
+      var output = util.format(
+        '✘ [%s] "%s": %s', result.testCase.id, result.testCase.in.input,
+        result.msg
+      ).red;
+      console.error( output );
+      break;
+
+    case 'placeholder':
+      var output = util.format(
+        '! [%s] "%s": %s', result.testCase.id, result.testCase.in.input,
+        result.msg
+      ).yellow;
+      console.error( output );
+      break;
+
+    default:
+      console.error(
+        util.format( 'Result type `%s` not recognized.', result.result )
+      );
+      process.exit( 1 );
+      break;
+  }
+
+  return output;
+}
 
 function equalProperties( expected, actual ){
   for( var prop in expected ){
@@ -22,20 +67,23 @@ function evalTest( priorityThresh, testCase, apiResults ){
   var expected;
   if( typeof testCase.out === 'string' ){
     if( testCase.out in locations ){
-      expected = locations;
+      expected = locations[ testCase.out ];
     }
     else {
       return {
-        success: false,
+        result: 'placeholder',
         msg: 'Placeholder test, no `out` object matches in `locations.json`.'
       }
     }
   }
   else if( testCase.out === null ){
     return {
-      success: false,
+      result: 'placeholder',
       msg: 'Placeholder test, no `out` specified.'
     };
+  }
+  else {
+    expected = testCase.out;
   }
 
   for( var ind = 0; ind < apiResults.length; ind++ ){
@@ -43,16 +91,16 @@ function evalTest( priorityThresh, testCase, apiResults ){
     if( equalProperties( expected, result.properties ) ){
       var success = ( ind + 1 ) <= priorityThresh;
       return ( success ) ?
-        { success: true } :
+        { result: 'pass' } :
         {
-          success: false,
-          msg: 'Result found, but not in top ' + priorityThresh
+          result: 'fail',
+          msg: util.format( 'Result found, but not in top %s.', priorityThresh )
         }
     }
   }
 
   return {
-    success: false,
+    result: 'fail',
     msg: 'No result found.'
   }
 }
