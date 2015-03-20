f20e0b8c9d73f2e273c966f801b94bb4415ca576
Severyn Kozak <severyn.kozak@gmail.com>
2015-03-20 15:53:41 -0400

    Add code to send an email.
    output_generators/email.js
    	-Add all the code necessary to send test-results out in an
    	email.
    
    package.json
    	-Add new dependencies.

--- output_generators/email.js
+++ output_generators/email.js
@@ -3,6 +3,8 @@ var fs = require( 'fs' );
 var path = require( 'path' );
 var outputJson = require( '../output2.json' );
 var util = require( 'util' );
+var nodemailer = require( 'nodemailer' );
+var juice = require( 'juice' );
 
 handlebars.registerHelper( 'json', JSON.stringify );
 handlebars.registerHelper( 'testCase', function ( res ){
@@ -32,8 +34,31 @@ handlebars.registerHelper( 'testCase', function ( res ){
   return out;
 });
 
-var emailTemplate = fs.readFileSync( path.join( __dirname, 'email.html' ) ).toString();
-console.log( handlebars.compile( emailTemplate )( outputJson ) );
+var emailTemplate = fs.readFileSync( path.join( __dirname, 'email_static/email.html' ) ).toString();
+var emailHtml = juice( handlebars.compile( emailTemplate )( outputJson ) );
+var transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: '',
+    pass: ''
+  }
+});
+
+var emailOpts = {
+  from: 'Severyn Kozak',
+  to: 'severyn.kozak@gmail.com',
+  subject: '2 pelias acceptance-tests results',
+  html: emailHtml
+};
+
+transporter.sendMail( emailOpts, function( err, info ){
+  if( err ){
+    console.error( err );
+  }
+  else {
+    console.log( 'Sent: ', info );
+  }
+});
 // module.exports = function ( suiteResults ){
   // console.log( handlebars.compile( emailTemplate )( suiteResults ) );
 // };
