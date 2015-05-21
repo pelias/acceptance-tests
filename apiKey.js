cc7b7870a233d13015251943587a281aaa8f5712
Peter Johnson <insomnia@rcpt.at>
2015-05-21 13:51:50 +0200

    remove debug

--- apiKey.js
+++ apiKey.js
@@ -5,7 +5,6 @@ var url = require('url'),
 module.exports = function( uri ){
 
   var settings = config.generate();
-  console.log( 'a', settings );
 
   if( settings && settings.hasOwnProperty('mapzen') ){
     var host = url.parse( uri ).host;
