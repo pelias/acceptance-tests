6623cd59613168f53fb506e85d3f9c039af1d4ab
Diana Shkolnikov <diana@mapzen.com>
2015-06-15 14:10:40 -0400

    Remove commented out error output

--- run_tests.js
+++ run_tests.js
@@ -194,7 +194,6 @@ function execTestSuite( apiUrl, testSuite, cb ){
         return;
       }
       else if( res.statusCode === 413 || res.statusCode === 429 ){
-        //console.error( 'Rate limit breached, rerunning.' );
         testSuite.tests.push( testCase );
         return;
       }
