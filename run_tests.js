9194bfb02377edb5dd5434d2061987294478a9f7
Harish Krishna <harish.krsn@gmail.com>
2015-07-14 14:31:10 -0400

    adding 408 to the list of tolerable codes

--- run_tests.js
+++ run_tests.js
@@ -194,12 +194,12 @@ function execTestSuite( apiUrl, testSuite, cb ){
         console.error( err );
         return;
       }
-      else if( res.statusCode === 413 || res.statusCode === 429 ){
+      else if( res.statusCode === 408 || res.statusCode === 413 || res.statusCode === 429 ){
         testSuite.tests.push( testCase );
         return;
       }
       else if( res.statusCode !== 200 ){
-        console.error( 'Non-{200,413} status code %s, exiting.\n'.red, res.statusCode );
+        console.error( 'Non-{200,408,413,429} status code %s, exiting.\n'.red, res.statusCode );
         console.error(
           'Failed for test case:\n%s\n',
           JSON.stringify( testCase, undefined, 4 ).replace( /^|\n/g, '\n\t' )
